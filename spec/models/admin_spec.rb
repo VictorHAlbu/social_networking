4c66e1c11a6f7473749d58561b1d206a5b4e4bd4
VictorHAlbu <vh12albuquerque@gmail.com>
2021-03-19 20:15:26 -0300

    testando validade de user e senha e não validade

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "admin is valid" do
+    admin = Admin.new(email: "vh12albuquerque@gmai.com", password: "123456")
+
+    expect(admin.email).to eql "vh12albuquerque@gmai.com"
+    expect(admin).to be_valid
+  end
+
+  it "admin is not valid" do
+    admin = Admin.new(email: "vh12albuquerque@gmai.com")
+    expect(admin).to_not be_valid
+  end 
+
 end
