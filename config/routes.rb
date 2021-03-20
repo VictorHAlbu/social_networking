6f335ea648329c43aac8d56cee594959f4d75d95
VictorHAlbu <vh12albuquerque@gmail.com>
2021-03-20 14:39:36 -0300

    Controler sessions redirecionando para login ao sair Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, skip: [:registrarions]
+  devise_for :admins, skip: [:registrarions], controllers: {sessions: "admins/sessions"}
  
   namespace :admin do
     root to: "home#index"
