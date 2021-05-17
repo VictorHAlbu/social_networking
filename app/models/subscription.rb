321b96fdffc9be7a1aaff9844645ab3b62af1860
VictorHAlbu <vh12albuquerque@gmail.com>
2021-05-17 13:48:30 -0300

    Gerando usuarios randomicos para testar subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,6 @@
 class Subscription < ApplicationRecord
-   belongs_to :followed, class_name: 'User'
-   belongs_to :followed_by, class_name: 'User'
+   belongs_to :followed, class_name: 'User' #seguido
+   belongs_to :followed_by, class_name: 'User'#quem segue
+   #duas foreign_key 
 end
   
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :user do
         name {"Victor"}
-        email {"vh12@gmail.com"}
+        email { "user#{rand(1..1000)}@user.com" }
         phone {"99355570"}
         avatar {Rack::Test::UploadedFile.new(Rails.root.join('spec/support/test-avatar.png'), 'image/png')}
         password {"11122233344"}
