af1c8ca28cd5eeaac631601726fd869fd65fb125
VictorHAlbu <vh12albuquerque@gmail.com>
2021-05-18 16:32:21 -0300

    Posts listados na timeline e função de excluir post

--- app/controllers/user/timeline_controller.rb
+++ app/controllers/user/timeline_controller.rb
@@ -1,7 +1,9 @@
 class User::TimelineController < UserController
   
     def index
-        
+        @posts =  Post.where(user_id: current_user.following.pluck(:id)).order(id: :desc)
+                            #pega todos ids das pessoas que current_user(eu) segue
+        #@posts esta recebendo os posts dos usuarios que o usuario atual segue 
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,10 @@ Rails.application.routes.draw do
   namespace :user do
    get 'profile', to: "profile#show"
    get 'potential_to_follow', to: "profile#potential_to_follow"
-   resources :posts, only: [:create] 
    get 'following', to: "profile#following"
    get 'followers', to: "profile#followers"
-
+   resources :posts, only: [:create, :destroy]  
+   resources :users, only: :show 
 
    post "follow/:id", to: "subscription#follow", as: :follow
    post 'unfollow/:id', to: "subscription#unfollow", as: :unfollow
